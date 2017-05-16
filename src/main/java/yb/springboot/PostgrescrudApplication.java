69c2f6a2bd3c07f148e5f2b71639b69e040fa801
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-16 12:04:06 +0530

    First Commit

---
+++ src/main/java/yb/springboot/PostgrescrudApplication.java
@@ -0,0 +1,12 @@
+package yb.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgrescrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgrescrudApplication.class, args);
+	}
+}
